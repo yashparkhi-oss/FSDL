0b8f5cfa0bb4694068c00f79eed3ca9b9bbecee9
yashparkhi-oss <yash.parkhi@mitwpu.edu.in>
2026-03-31 12:24:22 +0530

    Delete Interests.js

--- Interests.js
+++
@@ -1,9 +0,0 @@
-function Interests() {
-  return (
-    <section>
-      <h2>Interests</h2>
-      <p>Software Development | Database Management | Machine Learning | Problem Solving</p>
-    </section>
-  );
-}
-export default Interests;
