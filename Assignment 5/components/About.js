367d9cb5547e49d5602252f69e42e5304736459c
yashparkhi-oss <yash.parkhi@mitwpu.edu.in>
2026-03-31 12:23:08 +0530

    Add files via upload

---
+++ Assignment 5/components/About.js
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Computer Science Engineering student at MIT WPU with interest in 
+        software development, machine learning, and building efficient systems. 
+        Passionate about problem solving and real-world applications.
+    </p>
+    </section>
+  );
+}
+export default About;
